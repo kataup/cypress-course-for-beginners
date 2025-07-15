7a18c40f4e4872c2ed5dca7f2870192330f8eae3
Katerina Ulicna <k.ulicna@seznam.cz>
2025-07-15 19:54:33 +0200

    frgr

--- lessons/lesson-8/cypress-test/cypress/e2e/testK.cy.js
+++ lessons/lesson-8/cypress-test/cypress/e2e/testK.cy.js
@@ -1,9 +1,38 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://127.0.0.1:5500/cypress-course-for-beginners/lessons/lesson-8/exercise/login.html')
 
-
+// should čeká timeout, expect ihned porovná
   // ze prvek existuje v DOM
-  cy.get('selector').should('exist')
-  })
+  // cy.get('selector').should('exist')
+/*   cy.get('[data-testid="page-title"]').should('exist')
+  cy.get('[data-testid="page-title"]').should('have.text', 'Prihlásenie') //doslovný text
+  cy.get('[data-testid="page-title"]').should('contain.text', 'Pri') // obsahuje část textu
+  cy.get('[data-testid="page-title"]').should('contain.value', ''). type('K')
+  cy.get('[data-testid="page-title"]').should('have.value', 'Pri') */
+// have.attr , href
+
+// arange
+
+// act
+
+// assert 
+
+
+cy.get('[data-testid="username-input"]').should('exist')
+cy.get('[data-testid="username-input"]').should('be.visible')
+cy.get('[data-testid="login-button"]').should('be.disabled')
+
+cy.get('[data-testid="username-input"]').clear().type('nick')
+cy.get('[data-testid="username-input"]').clear().type('heslo')
+cy.get('[data-testid="login-button"]').should('be.enabled')
+
+cy.get('[data-testid="username-input"]').clear().type('nick')
+cy.get('[data-testid="username-input"]').clear().type('heslo')
+cy.get('[data-testid="reset-button"]').click()
+
+cy.get('[data-testid="username-input"]').clear().type('nick')
+cy.get('[data-testid="username-input"]').clear().type('heslo')
+cy.get()
+})
 })
