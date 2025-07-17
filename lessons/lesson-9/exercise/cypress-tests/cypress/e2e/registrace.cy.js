871666cf4f454240de8cc1b013f9f8d4cb6343d9
Katerina Ulicna <k.ulicna@seznam.cz>
2025-07-17 20:32:06 +0200

    jo

---
+++ lessons/lesson-9/exercise/cypress-tests/cypress/e2e/registrace.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
+
+describe('registrace', () => {
+    it('faker registrace', () => {
+        cy.visit('http://127.0.0.1:5500/cypress-course-for-beginners/lessons/lesson-9/exercise2/login.html')
+        
+        const userName = faker.internet.username()
+        const userEmail = faker.internet.email()
+        const userPassword = faker.internet.password()
+        
+        cy.log(userName)
+        cy.log(userEmail)
+        cy.log(userPassword)
+
+        cy.get()
+
+
+
+    })
+})
