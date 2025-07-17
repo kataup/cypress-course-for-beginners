1e5a6e7b89fa48d27d95a62a8daa37ee91510914
Katerina Ulicna <k.ulicna@seznam.cz>
2025-07-17 18:54:25 +0200

    b

--- lessons/lesson-9/exercise/cypress-tests/cypress/e2e/tests.cy.js
+++ lessons/lesson-9/exercise/cypress-tests/cypress/e2e/tests.cy.js
@@ -2,7 +2,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
 
-    cy.fixture('example.json')
+    cy.fixture('example.json').then((example)) // cypress najde obsah a vlozi ji do te promenne example
   })
 })
 
