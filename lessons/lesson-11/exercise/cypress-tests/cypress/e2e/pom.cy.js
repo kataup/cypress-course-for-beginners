35c56d8cdf9386bbf6cdb6f633c8d9884f857d0d
Katerina Ulicna <k.ulicna@seznam.cz>
2025-07-24 19:59:00 +0200

    oki

--- lessons/lesson-11/exercise/cypress-tests/cypress/e2e/pom.cy.js
+++ lessons/lesson-11/exercise/cypress-tests/cypress/e2e/pom.cy.js
@@ -1,15 +1,15 @@
-import loginPage from '../pages/loginPage'
+import loginPage from "../pages/loginPage"
 
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/login')
 
 /* loginPage.getEmailInput().clear().type('totojeemail@email.com')
 loginPage.getPasswordInput().clear().type('Heslo1234!')
 loginPage.getLoginButton().click() */
 
 loginPage.login('totojeemail@email.com', 'Heslo1234!')
-loginPage.login.getLoginButton().click()
+loginPage.getEmailInput().type('totojeemail@email.com')
 
 /* login(emailValue, passwordValue) {
         this.getEmailInput().type(emailValue)
