d397aad72e7a7b7ba2239d5ddb1075b3545e3db6
Katerina Ulicna <k.ulicna@seznam.cz>
2025-08-07 21:07:01 +0200

    lekce14

---
+++ lessons/lesson-14/exercise/cypress-tests/cypress.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from "cypress";
+import { readFileSync, writeFileSync} from 'fs'
+
+export default defineConfig({
+  watchForFileChanges: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+
+     let sharedDataText = ''
+
+      on('task', { 
+        saveSharedData(testshareddata: string) {
+          console.log('savind shared data:', testshareddata)
+          sharedDataText = testshareddata
+          return null 
+        },
+
+      getSharedData() {
+        return sharedDataText
+      },
+
+      saveSharedDataToFile(testshareddata: string) {
+        writeFileSync('cypress/fixtures/shareData.txt', testshareddata, 'utf8')
+        return null
+
+      },
+      getSharedDateFromFile() {
+        const data = readFileSync('cypress/fixtures/sharedData.txt', 'utf8')
+        return data
+
+      }
+      })
+    },
+  },
+});
---
+++ lessons/lesson-14/exercise/cypress-tests/cypress/e2e/spec.cy.ts
@@ -0,0 +1,37 @@
+
+describe('template spec', () => {
+  it('generate value', () => {
+    cy.visit('http://localhost:8080/')
+
+    cy.get('[data-testid="nav-generate-button"]').click()
+    cy.get('[data-testid="input1"]').type('ahoj')
+    cy.get('[data-testid="confirm-button"]').click() 
+    cy.get('[data-testid="result-text"]').then(($result) => {
+      const generetedText = $result.text().replace('Result 1:', '').trim()
+  
+      cy.task('saveSharedData', generetedText)
+
+        cy.task('saveShareDataToFile', generetedText)
+        cy.task('getSharedDataFromFile').then ((generovanyText) => {
+          cy.log('RETRIEVED SHARED DATA FROM FILE:' + generetedText)
+        })
+    })
+  })
+
+  it('validate value', () => {
+    cy.visit('http://localhost:8081')
+
+    cy.task('getSharedData').then((sharedData) => {
+  cy.get('[data-testid="nav-validate-button"]').click()
+
+      const textData = sharedData
+      cy.get('input[data-testid="validate-input"]').type(textData)
+      cy.get('[data-testid="verify-button"]').click()
+      cy.get('[data-testid="verified-text"]').should('contain', textData)
+
+    })
+    
+   // cy.get('button[data-testid="verify-button"').click()
+   //  cy.get('[data-testid="verified-text"]').should('contain', 'Verified')
+    }) 
+  })
